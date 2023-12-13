79ff4ebcb3a0d4173fcb73f7579f199e27e23d36
Hmendez19 <herwinmendezm@gmail.com>
2023-12-13 10:10:15 -0600

    feat(Spinner.tsx): adding Spinner component

---
+++ src/Shared/Views/Components/Spinner/Spinner.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spinner: React.FC = () => {
+    return (
+        <div className='display-center'>
+            <div className='spinner'></div>
+        </div>
+    );
+};
+
+export default Spinner;
