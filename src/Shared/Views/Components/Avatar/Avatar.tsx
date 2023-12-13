6fd3222eabc5d2d0c3376de86a375c1958e559d6
Hmendez19 <herwinmendezm@gmail.com>
2023-12-13 10:07:26 -0600

    feat(Avatar.tsx): adding Avatar component

---
+++ src/Shared/Views/Components/Avatar/Avatar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Avatar.module.css";
+import { ImageProp } from "../../../Interfaces/ImageProp";
+
+const Avatar: React.FC<ImageProp> = ({src, alt,size}) => {
+    return (
+        <div className={`${styles.avatar} ${styles["avatar--"+size]} ${styles["avatar__img--"+size]}`}>
+            <picture>
+               <img className={`${styles['avatar__img']} ${styles["avatar__img--"+size]}`} src={src} alt={alt} />
+            </picture>
+        </div>
+    )
+}
+  
+export default Avatar;
