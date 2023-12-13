a8b1bc642e9f7c81d166142728091b9269a53c01
Hmendez19 <herwinmendezm@gmail.com>
2023-12-13 10:34:19 -0600

    feat (Mixed.ts): adding constant mixed

---
+++ src/Shared/Constants/Mixed.ts
@@ -0,0 +1,9 @@
+export const TotalResult:number = 100;
+
+export const DeveloperName:string = "Herwin Mendez";
+
+export const AppName:string = "Random Users";
+
+export const FooterDescription:string = "HAMM. All rights reserved.";
+
+export const ColumnNames:string[] = ['Imágen','Nombre completo','Email','País','Acción'];
