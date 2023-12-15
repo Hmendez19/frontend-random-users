5bee8cbc9e75964041cb6d1ed7805b7ab40899cf
Hmendez19 <herwinmendezm@gmail.com>
2023-12-15 14:05:09 -0600

    feat: adding UserId value object into delete user (use case)

--- src/Modules/Users/Application/UseCase/Delete/DeleteUser.ts
+++ src/Modules/Users/Application/UseCase/Delete/DeleteUser.ts
@@ -1,7 +1,9 @@
 import { User } from "../../../Domain/Entities/UserEntity";
 import { UserRepository } from "../../../Domain/Repositories/UserRepository";
+import { UserId } from "../../../Domain/ValuesObject/UserId";
 
 
 export function DeleteUser(userRepository:UserRepository,userId:string):User[]|[]{
-    return userRepository.DeleteUser(userId);
+    const userIdValueObject=new UserId(userId);
+    return userRepository.DeleteUser(userIdValueObject.getValue());
 }
